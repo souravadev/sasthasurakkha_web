0ff2c2954deca4cef1f4dcb82985c2159e4b5e6a
Sourav Adhikari <souravadhikari670@gmail.com>
2025-09-07 09:30:10 +0530

    update

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.components.layout')
+
+@section('title', 'Admin Home')
+
+@section('content')
+    <div style="margin-top: 100px">
+        <h1>Welcome to Admin pages</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function() {
+    Route::get('/home', function() {
+        return view('admin/home');
+    });
+
+    Route::prefix('users')->group(function() {
+        Route::get('/add', function() {
+            return view('admin/users/add-user');
+        });
+    });
+
+    Route::prefix('doctors')->group(function() {
+        Route::get('/add', function() {
+            return view('admin/doctors/add-doctor');
+        });
+    });
+});
