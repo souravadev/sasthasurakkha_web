6311a576d8fa32cbd7375d1ac86fb8b4d97ad638
Sourav Adhikari <souravadhikari670@gmail.com>
2025-09-09 07:33:19 +0530

    update

--- resources/views/admin/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.components.layout')
-
-@section('title', 'Admin Home')
-
-@section('content')
-    <div style="margin-top: 100px">
-        <h1>Welcome to Admin pages</h1>
-    </div>
-@endsection
